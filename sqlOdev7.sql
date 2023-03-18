160bd9f8c66362d5ac833aa7f9f36d037e8b4684
ertug-guney <yukselenay@gmail.com>
2023-03-18 23:59:16 +0300

    Second commit

--- sqlOdev7.sql
+++ sqlOdev7.sql
@@ -12,4 +12,5 @@ GROUP BY store_id;
 -- 4. city tablosunda bulunan şehir verilerini country_id sütununa göre gruplandırdıktan sonra en fazla şehir sayısı barındıran country_id bilgisini ve şehir sayısını paylaşınız.
 SELECT country_id, COUNT(DISTINCT city) FROM city
 GROUP BY country_id
-ORDER BY COUNT(DISTINCT city) DESC;
+ORDER BY COUNT(DISTINCT city) DESC
+LIMIT 1;
